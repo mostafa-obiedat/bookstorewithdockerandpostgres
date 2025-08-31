933400d75573e1033917c6e0fb97a2a0876ccf19
mostafa-obiedat <mustafaobiedat3@gmail.com>
2025-08-31 18:05:06 +0300

    ch15- handle search/filter form for books

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
-urlpatterns = [
+urlpatterns = [     
+  path("search/", SearchResultsListView.as_view(), name="search_results"),
   path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),  
   path("", BookListView.as_view(), name="book_list"),
 ]

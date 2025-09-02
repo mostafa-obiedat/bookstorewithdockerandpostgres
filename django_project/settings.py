e3b520b0c8000f274ee04960caa2403fb722a778
mostafa-obiedat <mustafaobiedat3@gmail.com>
2025-09-02 15:05:16 +0300

    ch17- create production confic and test it, and adding a security layers to site

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG") 
+DEBUG = env.bool("DJANGO_DEBUG", default=False) # new
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"] # new
 
@@ -177,3 +177,17 @@ DEFAULT_FROM_EMAIL = "admin@djangobookstore.com" # new
 import socket
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+
+
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
+# HTTP Strict Transport Security (HSTS) config
+SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=2592000) # 30 days
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
+default=True)
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+
+
+# Secure Cookies
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
